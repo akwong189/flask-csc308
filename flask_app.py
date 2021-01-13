587e6db420dde4dfb45fd64fb7ca5cd7adf9af53
Alterac8 <awong189@calpoly.edu>
2021-01-13 12:15:07 -0800

    init commit

---
+++ flask_app.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+users = { 
+    'users_list' :
+    [
+        { 
+            'id' : 'xyz789',
+            'name' : 'Charlie',
+            'job': 'Janitor',
+        },
+        {
+            'id' : 'abc123', 
+            'name': 'Mac',
+            'job': 'Bouncer',
+        },
+        {
+            'id' : 'ppp222', 
+            'name': 'Mac',
+            'job': 'Professor',
+        }, 
+        {
+            'id' : 'yat999', 
+            'name': 'Dee',
+            'job': 'Aspring actress',
+        },
+        {
+            'id' : 'zap555', 
+            'name': 'Dennis',
+            'job': 'Bartender',
+        }
+    ]
+}
+
+@app.route('/')
+def hello_world():
+        return 'Hello, World!'
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        search_job = request.args.get('job')
+        if search_username:
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username and not search_job:
+                    subdict['users_list'].append(user)
+                elif user['name'] == search_username and user['job'] == search_job:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        return resp
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):
+    if id :
+        if request.method == 'GET':
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        elif request.method == 'DELETE':
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+                    resp = jsonify(), 204
+                    return resp
+            resp = jsonify({"msg:": "user id not found"}), 404
+            return resp
+    return users
